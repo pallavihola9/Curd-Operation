3382a7feb600ae37d24916038ca4a22f0e7b5555
pallavi1916 <rautpallavi1916@gmail.com>
2023-05-03 13:23:43 +0530

    Max Length Change Commit

--- Authentication_Page/Curd/models.py
+++ Authentication_Page/Curd/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    srnumber= models.CharField(max_length=50)
-    fname= models.CharField(max_length=50)
-    lname= models.CharField(max_length=50)
+    srnumber= models.CharField(max_length=150)
+    fname= models.CharField(max_length=100)
+    lname= models.CharField(max_length=100)
     email= models.CharField(max_length=100)
     address= models.CharField(max_length=200)
     phonenumber= models.CharField(max_length=10)
